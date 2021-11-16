2ba1513a9081ab56826786348fffdac1b35a9cdb
aj <404americajones@gmail.com>
2021-11-16 14:57:52 -0700

    added ZH section

--- script.js
+++ script.js
@@ -8,26 +8,49 @@ let num = 6;
 let numberOfWords = 25;
 // let letterListArr = ["a", "s", "d", "f", "j", "k", "l", "e", "n", "i", "o", "b", "u", "t"];
 let fullLetterList = "enitrlsauodychgmpbkvwfzxqj";
+let fullLetterListZH = "ㄧㄕㄝㄨㄉㄢㄒㄥㄅㄤㄑㄡㄐㄟㄌㄊㄜㄓㄆㄠㄩㄘㄣㄈㄔㄖㄇㄏㄋㄍㄗㄚㄎㄙㄛㄦ";
 let wordArray;
 let toTypeArr = [];
 let nuTypeArr = [];
 let wholeString = "";
 let thisNum = 0;
 let testingChar;
+let ZH = false;
+if (ZH) {
+    num = 8;
+}
 function letterCountDown() {
-    if (num === 5) {
-        return
+    if (ZH) {
+        if (num === 8) {
+            return
+        } else {
+            num--;
+            pickWordsZH();
+        }
     } else {
-        num--;
-        pickWords();
-    }
-};
+        if (num === 5) {
+            return
+        } else {
+            num--;
+            pickWords();
+        }
+    };
+}
 function letterCountUp() {
-    if (num === 26) {
-        return
+    if (ZH === true) {
+        if (num === 36) {
+            return
+        } else {
+            num++;
+            pickWordsZH();
+        }
     } else {
-        num++;
-        pickWords();
+        if (num === 26) {
+            return
+        } else {
+            num++;
+            pickWords();
+        }
     }
 }
 function wordCountDown() {
@@ -46,7 +69,40 @@ function wordCountUp() {
         pickWords();
     }
 }
-pickWords();
+if (ZH) {
+    pickWordsZH()
+} else {
+    pickWords();
+}
+function pickWordsZH() {
+    toTypeArr = [];
+    nuTypeArr = [];
+    removeAllChildren(toTypeBox);
+    let letterList = fullLetterListZH.substring(0, num);
+    numBox.textContent = num + " letters";
+    wordNumBox.textContent = numberOfWords + " words";
+    zhuyinWords.forEach(word => {
+        wordArray = [];
+        for (let i = 0; i < word.length; i++) {
+            console.log(letterList);
+            console.log(word[i]);
+            if (!letterList.includes(word[i])) {
+                // console.log("YES: ", word);
+                // wordArray.push(word);
+                return;
+            } else {
+                wordArray.push(word);
+            }
+        }
+        // console.log(wordArray)
+        // console.log(wordArray.length)
+        if (wordArray.length === word.length) {
+            // console.log(word);
+            toTypeArr.push(word);
+        };
+    });
+    shuffleAndPostZH();
+}
 function pickWords() {
     toTypeArr = [];
     nuTypeArr = [];
@@ -100,6 +156,31 @@ function shuffleAndPost() {
     })
     initiateTest();
 }
+function shuffleAndPostZH() {
+    // console.log("before: ", toTypeArr);
+    shuffleArray(toTypeArr);
+    // console.log("after ", toTypeArr);
+    for (let i = 0; i < numberOfWords; i++) {
+        nuTypeArr.push(toTypeArr[i])
+        // console.log(toTypeArr[i]);
+    }
+    nuTypeArr.forEach(word => {
+        let nuDiv = document.createElement('div');
+        nuDiv.textContent = word + "_";
+        toTypeBox.append(nuDiv);
+    });
+    wholeString = toTypeBox.textContent;
+    let nuArr = wholeString.split("");
+    removeAllChildren(toTypeBox);
+    removeAllChildren(heartBox);
+    nuArr.forEach(letter => {
+        let nuItem = "<span class='textInputItem'>" + letter + "</span>";
+        let nuDiv = document.createElement('div');
+        nuDiv.innerHTML = nuItem;
+        toTypeBox.append(nuDiv)
+    })
+    initiateTestZH();
+}
 function initiateTest() {
     thisNum = 0;
     // console.log(toTypeBox.textContent.charAt(thisNum));
@@ -109,6 +190,20 @@ function initiateTest() {
         // console.log(event.key);
         if (event.key === testingChar) {
             continueTest();
+        }
+    }, { once: true });
+};
+const ZHkeyboard = {}
+//comparing keyboard key inputs to zhuyin characters
+function initiateTestZH() {
+    thisNum = 0;
+    // console.log(toTypeBox.textContent.charAt(thisNum));
+    toTypeBox.children[thisNum].classList.add('white');
+    testingChar = toTypeBox.textContent.charAt(thisNum);
+    document.addEventListener("keyup", (event) => {
+        // console.log(event.key);
+        if (event.key === testingChar) {
+            continueTestZH();
         }
     }, { once: true });
 };
@@ -122,17 +217,18 @@ function continueTest() {
         }, 3400);
     } else {
         thisNum++;
-        console.log("num is now: ", thisNum);
+        // console.log("num is now: ", thisNum);
         toTypeBox.children[thisNum - 1].classList.remove('white');
         toTypeBox.children[thisNum].classList.add('white');
         testingChar = toTypeBox.textContent.charAt(thisNum);
         document.addEventListener("keyup", (event) => {
-            // console.log(event.key);
+            console.log(event.key);
             // console.log("SHOULD BE:", testingChar);
             if (testingChar === "_" && event.code === "Space") {
                 addHeart();
                 continueTest();
             } else if (event.key === testingChar) {
+                addStar();
                 continueTest();
             } else { return }
         })
@@ -143,6 +239,15 @@ var winWidth = window.innerWidth;
 function addHeart() {
     let nuDiv = document.createElement('div');
     nuDiv.classList.add('pixelheart');
+    randomTop = getRandomNumber(0, winHeight);
+    randomLeft = getRandomNumber(0, winWidth);
+    nuDiv.style.top = randomTop + 'px';
+    nuDiv.style.left = randomLeft + 'px';
+    heartBox.append(nuDiv)
+}
+function addStar() {
+    let nuDiv = document.createElement('div');
+    nuDiv.classList.add('blips');
     randomTop = getRandomNumber(0, winHeight);
     randomLeft = getRandomNumber(0, winWidth);
     nuDiv.style.top = randomTop + 'px';
